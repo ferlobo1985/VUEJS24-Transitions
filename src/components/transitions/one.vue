35e77019bcae647efbf7a37f320e89b0f83ce0ff
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-08 18:56:43 -0300

    01-Common Transition
    01-Common Transition

--- src/components/transitions/one.vue
+++ src/components/transitions/one.vue
@@ -1,13 +1,28 @@
 <template>
-    <div class="p-3 mb-2 bg-success text-white"
-        v-if="data.display"
-    >
-        Hello
-    </div>
+    <transition>
+        <div class="p-3 mb-2 bg-success text-white"
+            v-if="data.display"
+        >
+            Hello
+        </div>
+    </transition>
     <button class="btn btn-primary" 
         @click="data.display = !data.display"
     >
         Toggle
+    </button>
+    <hr/>
+    <transition>
+        <div class="p-3 mb-2 bg-warning text-white"
+            v-if="data.display2"
+        >
+            Hello
+        </div>
+    </transition>
+    <button class="btn btn-primary" 
+        @click="data.display2 = !data.display2"
+    >
+        Toggle display 2
     </button>
 </template>
 
@@ -15,6 +30,26 @@
    import { reactive } from 'vue';
 
    const data = reactive({
-    display:false
+    display:false,
+    display2: true
    })
 </script>
+
+<style scoped>
+
+    .v-enter-from,
+    .v-leave-to {
+        opacity: 0;
+    }
+
+    .v-enter-active,
+    .v-leave-active {
+        transition: 5s;
+    }
+
+    .v-enter-to,
+    .v-leave-from {
+        opacity: 1;
+    }
+
+</style>
