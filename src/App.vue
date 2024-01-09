d9d6d7040820421b9f2c25a109dbb50a5b32863d
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-08 22:31:54 -0300

    03-Transition mode
    03-Transition mode

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <app-header></app-header>
       <div class="container">
-        <One/>
+        <Two/>
       </div>
 </template>
 
 <script setup>
-  import One from '@/components/transitions/one.vue';
-  //import Two from '@/components/transitions/two.vue'
+  // import One from '@/components/transitions/one.vue';
+  import Two from '@/components/transitions/two.vue'
 </script>
--- src/components/transitions/two.vue
+++ src/components/transitions/two.vue
@@ -1,20 +1,51 @@
 <template>
-    <div class="p-3 mb-2 bg-success text-white"
-        v-if="data.display"
-    >
-        Hello
-    </div>
-    <button class="btn btn-primary" 
-        @click="data.display = !data.display"
-    >
-        Toggle
+    <button class="btn btn-primary"  @click="status = !status">
+        Toggle status
     </button>
+
+    <transition 
+        mode="out-in"
+        name="custom"
+        enter-active-class="dog"
+        leave-active-class="cat"
+    >
+        <div class="p-3 mb-2 bg-danger text-white"
+            v-if="!status"
+            key="status_off"    
+        >
+            OFF
+        </div>
+        <div class="p-3 mb-2 bg-success text-white"
+            v-else="status"
+            key="status_on"    
+        >
+            ON
+        </div>
+    </transition>
+   
 </template>
 
 <script setup>
-   import { reactive } from 'vue';
-
-   const data = reactive({
-    display:false
-   })
+   import { ref } from 'vue';
+   const status = ref(false);
 </script>
+
+
+<style scoped>
+
+    .custom-enter-from,
+    .custom-leave-to {
+        opacity: 0;
+    }
+
+    .dog,
+    .cat {
+        transition: 1s;
+    }
+
+    .custom-enter-to,
+    .custom-leave-from {
+        opacity: 1;
+    }
+
+</style>
